2202dec5ec91426acc516e2590b6d8c2dca12a72
Gabriel Matos <gabriel@capitola.nl>
2025-02-20 17:47:51 +0100

    navbar v1

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 })->name('home');
+
+Route::get('/Evenementen', function () {
+    return view('Evenementen');
+})->name('Evenementen');
+
+Route::get('/Inloggen', function () {
+    return view('Inloggen');
+})->name('Inloggen');
