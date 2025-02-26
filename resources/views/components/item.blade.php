7194153159d742dfe52c6d0163ac12c2e99efd1c
ramon luteijn <ramon@intractief.nl>
2025-02-26 14:30:02 +0100

    ag-24 user styling

---
+++ resources/views/components/item.blade.php
@@ -0,0 +1,15 @@
+@props(['event'])
+
+<div class="card-container">
+    <div class="card">
+        <img src="{{ asset($event->image_url)}}" alt="{{ $event->title }}">
+        <h3>{{$event->title}}</h3>
+        <h6 class="no-margin">{{ucfirst($event->category->value)}}</h6>
+        <p>
+            {{Str::of($event->description)->words(20, '...')}}
+        </p>
+
+        {{--                    priority 2???--}}
+        <a href="{{ route('user.event.show', $event->id) }}">Lees verder</a>
+    </div>
+</div>
--- resources/views/user/events/index.blade.php
+++ resources/views/user/events/index.blade.php
@@ -3,20 +3,9 @@
 @section("title", "Events")
 
 @section("content")
-    <div>
+    <div id="post-list" class="flex horizontal wrap centered stretch">
         @foreach($events as $event)
-            <div>
-                <div>
-                    <img src="{{ asset($event->image_url)}}" alt="{{ $event->title }}">
-                </div>
-                <div>
-                    <h2>{{$event->title}}</h2>
-                    <p>{{Str::of($event->description)->words(20, '...')}}</p>
-                </div>
-                <div>
-                    <a href="{{ route('user.event.show', $event->id) }}">View Details</a>
-                </div>
-            </div>
+            <x-item :event="$event"/>
         @endforeach
     </div>
 @stop
