b2f98ca687e63256cda026066fe0f17757473c59
pashavdw <pasha.pasha12e@gmail.com>
2025-02-26 11:42:02 +0100

    WIP: Sponsor CRUD toegevoegd

---
+++ database/migrations/2025_02_26_103329_create_sponsors_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('sponsors', function (Blueprint $table) {
+            $table->id();
+            $table->string('logo');
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->boolean('active')->default(true);
+            $table->timestamps();
+        });
+
+        Schema::create('event_sponsor', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('sponsor_id')->constrained('sponsors')->onDelete('cascade');
+            $table->foreignId('event_id')->constrained('events')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('event_sponsor');
+        Schema::dropIfExists('sponsors');
+    }
+};
