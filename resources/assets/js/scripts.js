aa1ff3c5e293b31c9dfb571530b44662d7ca2f20
ramon luteijn <ramon@intractief.nl>
2025-03-10 14:35:23 +0100

    AG-29 simple modal styling

--- resources/assets/js/scripts.js
+++ resources/assets/js/scripts.js
@@ -2,6 +2,7 @@ import Swiper from "swiper/bundle";
 
 document.addEventListener('DOMContentLoaded', function() {
     carouselSwiper();
+    openModal();
 });
 
 function carouselSwiper() {
@@ -29,4 +30,30 @@ function carouselSwiper() {
             }
         }
     });
+}
+
+function openModal() {
+    const eventDateInput = document.getElementsByName('date')[0];
+    const openModalButton = document.getElementById('openModalButton');
+    const submitButton = document.getElementById('submitButton');
+    const modal = document.getElementById('dateModal');
+    const today = new Date().toISOString().split('T')[0];
+
+    function checkDate() {
+        const selectedDate = eventDateInput.value;
+        if (selectedDate < today) {
+            openModalButton.classList.remove('hidden');
+            submitButton.classList.add('hidden');
+        } else {
+            openModalButton.classList.add('hidden');
+            submitButton.classList.remove('hidden');
+        }
+    }
+
+    eventDateInput.addEventListener('change', checkDate);
+    openModalButton.addEventListener('click', function () {
+        modal.classList.toggle('hidden');
+    });
+
+    checkDate();
 }
--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -3,7 +3,7 @@
     "message" => "",
     "route" => ""
 ])
-<div id="dateModal" class="relative z-10 hidden" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+<div id="dateModal" class="relative z-10 hidden modal-wrapper" aria-labelledby="modal-title" role="dialog" aria-modal="true">
 
     <!--
       Background backdrop, show/hide based on modal state.
@@ -17,7 +17,7 @@
     -->
     <div class="fixed inset-0 bg-gray-500/75 transition-opacity" aria-hidden="true"></div>
 
-    <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
+    <div class="fixed inset-0 z-10 w-screen overflow-y-auto outer-wrapper">
         <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
             <!--
               Modal panel, show/hide based on modal state.
@@ -29,7 +29,7 @@
                 From: "opacity-100 translate-y-0 sm:scale-100"
                 To: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             -->
-            <div class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+            <div class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg inner-wrapper">
                 <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                     <div class="sm:flex sm:items-start">
                         <div class="mx-auto flex size-12 shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:size-10">
@@ -47,9 +47,20 @@
                 </div>
                 <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
                     <button type="submit" class="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-blue-500 sm:ml-3 sm:w-auto">Doorgaan</button>
-                    <button type="button" class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 shadow-xs ring-gray-300 ring-inset hover:bg-gray-50 sm:mt-0 sm:w-auto">Annuleren</button>
+                    <button id="closeModalButton" type="button" class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 shadow-xs ring-gray-300 ring-inset hover:bg-gray-50 sm:mt-0 sm:w-auto">Annuleren</button>
                 </div>
             </div>
         </div>
     </div>
 </div>
+
+<script>
+    document.addEventListener('DOMContentLoaded', function () {
+        const closeModalButton = document.getElementById('closeModalButton');
+        const modal = document.getElementById('dateModal');
+
+        closeModalButton.addEventListener('click', function () {
+            modal.classList.add('hidden');
+        });
+    });
+</script>
