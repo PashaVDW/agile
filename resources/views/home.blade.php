12aa20dbe21128147487303a4fcb4f4f81ca3502
Ikkie <ikkie857@outlook.com>
2025-03-10 09:34:52 +0100

    AG-62 Banner op desktop werkt

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,14 @@
 @section("title", "Home")
 
 @section("content")
-<div class="banner">
-    {{ $welkomsbericht }}
+<div class="concat-banner">
+    <div class="concat-title">
+        <div>
+            <h2>Welkom bij</h2>
+            <h1>Studievereniging<br/>Concat</h1>
+        </div>
+        <img src="https://svconcat.nl/media/assets/logo-white.svg" alt="Concat Logo">
+        <div class="real-pseudo-element"></div>
+    </div>
 </div>
 @stop
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
   <head>
+    <link href='https://fonts.googleapis.com/css?family=Quicksand' rel='stylesheet'>
     <title>{{ config("app.name") }} | @yield("title")</title>
     @vite(["resources/assets/js/app.js"])
   </head>
