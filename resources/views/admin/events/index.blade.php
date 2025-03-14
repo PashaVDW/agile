a755d8cf8e6786010ab53054d7304bbc3d058c2c
ramon luteijn <ramon@intractief.nl>
2025-03-14 17:15:40 +0100

    ag-84 updated translation + renamed form info

--- resources/views/admin/events/index.blade.php
+++ resources/views/admin/events/index.blade.php
@@ -6,7 +6,7 @@
     <div class="container">
         <div class="filter-wrapper">
             <form method="GET" action="{{ route(Route::currentRouteName()) }}">
-                <x-forms.input-select :onchange="'this.form.submit()'" label="Categorie" default="Alle categorieën" name="status" enum="{{\App\Enums\ActiveTypeEnum::class}}" value="{{ request('status') }}"/>
+                <x-forms.input-select :onchange="'this.form.submit()'" label="Status" default="Alle statussen" name="status" enum="{{\App\Enums\ActiveTypeEnum::class}}" value="{{ request('status') }}"/>
             </form>
             <a href="{{ route("admin.event.create") }}" class="button right">Event aanmaken</a>
         </div>
