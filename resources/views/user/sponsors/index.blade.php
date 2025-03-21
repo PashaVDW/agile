7583208f9d0035bcd3899c0c706c50c2e7cf2fb2
Ikkie <ikkie857@outlook.com>
2025-03-21 14:33:12 +0100

    AG-26 Pagina staat klaar om opgemaakt te worden

---
+++ resources/views/user/sponsors/index.blade.php
@@ -0,0 +1,13 @@
+@extends("layouts.default")
+
+@section("title", "Sponsors")
+
+@section("content")
+{{--    foreach sponsor--}}
+    @foreach($sponsors as $sponsor)
+        <div class="sponsor">
+            <h1 class="has-background">{{$sponsor->name}}</h1>
+            <p>{!! $sponsor->description !!}</p>
+        </div>
+    @endforeach
+@stop
