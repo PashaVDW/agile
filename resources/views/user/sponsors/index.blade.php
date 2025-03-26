e5373f4ee423f6486b31782fd21faac484f3b712
Ikkie <ikkie857@outlook.com>
2025-03-26 12:24:58 +0100

    AG-26 Sponsoren desktop opmaak verbeterd

--- resources/views/user/sponsors/index.blade.php
+++ resources/views/user/sponsors/index.blade.php
@@ -3,10 +3,19 @@
 @section("title", "Sponsors")
 
 @section("content")
-    @foreach($sponsors as $sponsor)
-        <div class="sponsor">
-            <h1 class="has-background">{{$sponsor->name}}</h1>
-            <p>{!! $sponsor->description !!}</p>
+    <div class="instruction-message">
+        <p>Wil je sponsor worden? Mail dan naar <a href="mailto:info@svconcat.nl">info@svconcat.nl</a></p>
+    </div>
+
+    <div class="sponsors-overview">
+        <div class="sponsors-container">
+            @foreach($sponsors as $sponsor)
+                <div class="sponsor">
+                    <img src="{{ asset($sponsor->image_url)}}" alt="{{ $sponsor->name }}" class="sponsor-image">
+                    <h1 class="has-background">{{$sponsor->name}}</h1>
+                    <p>{!! $sponsor->description !!}</p>
+                </div>
+            @endforeach
         </div>
-    @endforeach
+    </div>
 @stop
