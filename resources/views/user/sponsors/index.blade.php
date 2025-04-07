0951c07bfd71ee6975edb009b352e46721e9a71f
ramon luteijn <ramon@intractief.nl>
2025-04-07 19:43:51 +0200

    #98 fixed sponsor styling

--- resources/views/user/sponsors/index.blade.php
+++ resources/views/user/sponsors/index.blade.php
@@ -4,15 +4,17 @@
 
 @section("content")
     <div class="section">
-        <div class="instruction-message">
-            <p>Wil je sponsor worden? Mail dan naar <a href="mailto:info@svconcat.nl">info@svconcat.nl</a></p>
+        <div class="container">
+            <div class="instruction-message">
+                <p>Wil je sponsor worden? Mail dan naar <a href="mailto:info@svconcat.nl">info@svconcat.nl</a></p>
+            </div>
         </div>
 
         <div class="sponsors-overview">
             <div class="sponsors-container">
                 @foreach($sponsors as $sponsor)
                     <div class="sponsor">
-                        <a href="{{ $sponsor->url }}" target="_blank"><img src="{{ asset($sponsor->image_url)}}" alt="Logo van {{ $sponsor->name }}" class="sponsor-image"></a>
+                        <a href="{{ $sponsor->url }}" class="no-line" target="_blank"><img src="{{ asset($sponsor->image_url)}}" alt="Logo van {{ $sponsor->name }}" class="sponsor-image"></a>
                         <h1 class="has-background">{{$sponsor->name}}</h1>
                         <p>{!! $sponsor->description !!}</p>
                     </div>
