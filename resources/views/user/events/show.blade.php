2c34831edd3c7218a2e930e1e36538644a6ab7ec
ramon luteijn <ramon@intractief.nl>
2025-04-18 23:08:20 +0200

    #ag-89 added button styling

--- resources/views/user/events/show.blade.php
+++ resources/views/user/events/show.blade.php
@@ -40,16 +40,20 @@
                         <li><span>Betalen voor: </span><a href="{{$event->payment_link}}" target="_blank">{{$event->title}}</a></li>
                     @endif
                 </ul>
-                @if($event->is_open && auth()->user())
-                    <form action="{{ $event->isRegistered() ? route('user.event.unregister', $event->id) : route('user.event.register', $event->id) }}" method="POST">
-                        @csrf
-                        @if($event->isRegistered())
-                            @method('DELETE')
-                        @endif
-                        <button type="submit" class="item-button">
-                            {{ $event->isRegistered() ? 'Afmelden' : 'Inschrijven' }}
-                        </button>
-                    </form>
+                @if($event->is_open)
+                    @if(auth()->user())
+                        <form action="{{ $event->isRegistered() ? route('user.event.unregister', $event->id) : route('user.event.register', $event->id) }}" method="POST">
+                            @csrf
+                            @if($event->isRegistered())
+                                @method('DELETE')
+                            @endif
+                            <button type="submit" class="item-button">
+                                {{ $event->isRegistered() ? 'Afmelden' : 'Inschrijven' }}
+                            </button>
+                        </form>
+                    @else
+                        <a href="{{ route('login') }}" class="no-line button item-button">Inloggen</a>
+                    @endif
                 @endif
             </div>
         </div>
