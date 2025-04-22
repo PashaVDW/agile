e8b66ca464e0e04576b0e2f2659f719f58f34328
Pasha Ahmad <pasha.pasha12e@gmail.com>
2025-04-22 18:55:29 +0200

    button style in form.blade.php

--- resources/views/admin/announcements/form.blade.php
+++ resources/views/admin/announcements/form.blade.php
@@ -69,7 +69,7 @@
             <div class="mb-3">
                 <button
                     type="submit"
-                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    class="button"
                 >
                     {{ isset($announcement) ? 'Bijwerken' : 'Aanmaken' }}
                 </button>
