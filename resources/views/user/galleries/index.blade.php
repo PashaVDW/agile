045812a96d0aafad42ad0304a7a6e3a13622047a
Rens <rm.aarts@student.avans.nl>
2025-04-29 16:00:20 +0200

    Cleanup + Usage component gallerymodal.blade.php #58

--- resources/views/user/galleries/index.blade.php
+++ resources/views/user/galleries/index.blade.php
@@ -16,24 +16,7 @@
 
     @if($gallery->hasPhotos())
         @foreach($gallery->gallery as $image)
-            <div class="modal fade gallery-modal" id="exampleModal{{$loop->index}}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog modal-dialog-centered modal-lg">
-                    <div class="modal-content">
-                        <div class="modal-header justify-content-end">
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true" class="menu-close"></span>
-                            </button>
-                        </div>
-                        <div class="modal-body d-flex justify-center align-middle">
-                            <div class="w-100 h-100 overflow-hidden zoom-wrapper">
-                                <div class="d-flex justify-center area-wrapper">
-                                    <img src="{{ asset($gallery->getGalleryImagePath($image)) }}" class="d-block">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <x-gallerymodal :id="$loop->index" :gallery="$gallery" :image="$image"/>
         @endforeach
     @endif
 @stop
