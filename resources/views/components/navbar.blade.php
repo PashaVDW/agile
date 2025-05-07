7680214692e8e28ce01344d59f4b11696af9058b
ramon luteijn <ramon@intractief.nl>
2025-05-07 15:34:37 +0200

    fixed user navbar with submenus

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-xl navbar-light bg-light">
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -8,14 +8,16 @@
             <li class="nav-item {{ Request::is('/') ? 'active' : '' }}">
                 <a class="nav-link" href="{{ route('home') }}">Home</a>
             </li>
-            <li class="nav-item {{ Request::is('event*') ? 'active' : '' }}">
+            <li class="nav-item has-children {{ Request::is('event*') ? 'active' : '' }}">
                 <a class="nav-link" href="{{ route('user.events.index') }}">Evenementen</a>
+                <ul class="submenu">
+                    <li class="nav-item {{ Request::is('community*') ? 'active' : '' }}">
+                        <a class="nav-link" href="{{ route('user.community.index') }}">Community</a>
+                    </li>
+                </ul>
             </li>
             <li class="nav-item {{ Request::is('announcement*') ? 'active' : '' }}">
                 <a class="nav-link" href="{{ route('public.announcements.index') }}">Mededelingen</a>
-            </li>
-            <li class="nav-item {{ Request::is('community*') ? 'active' : '' }}">
-                <a class="nav-link" href="{{ route('user.community.index') }}">Community</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="https://sv-concat.myspreadshop.nl" target="_blank" rel="noopener noreferrer">Webshop</a>
@@ -24,30 +26,34 @@
                 <a class="nav-link" href="{{ route('user.sponsors.index') }}">Sponsoren</a>
             </li>
 
-            @auth
-                @if(auth()->user()->hasRole('admin'))
-                    <li class="nav-item {{ Request::is('admin') ? 'active' : '' }}">
-                        <a class="nav-link" href="{{ route('admin.index') }}">Admin</a>
+            <li class="nav-item has-children">
+                <span class="nav-link">Account</span>
+                <ul class="submenu">
+                    @auth
+                    @if(auth()->user()->hasRole('admin'))
+                        <li class="nav-item {{ Request::is('admin') ? 'active' : '' }}">
+                            <a class="nav-link" href="{{ route('admin.index') }}">Admin</a>
+                        </li>
+                    @endif
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('logout') }}"
+                           onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                            Uitloggen
+                        </a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
                     </li>
-                @endif
-
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('logout') }}"
-                       onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                        Uitloggen
-                    </a>
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                        @csrf
-                    </form>
-                </li>
-            @else
-                <li class="nav-item {{ Request::is('register') ? 'active' : '' }}">
-                    <a class="nav-link" href="{{ route('register') }}">Inschrijven</a>
-                </li>
-                <li class="nav-item {{ Request::is('login') ? 'active' : '' }}">
-                    <a class="nav-link" href="{{ route('login') }}">Inloggen</a>
-                </li>
-            @endauth
+                        @else
+                        <li class="nav-item {{ Request::is('register') ? 'active' : '' }}">
+                            <a class="nav-link" href="{{ route('register') }}">Inschrijven</a>
+                        </li>
+                        <li class="nav-item {{ Request::is('login') ? 'active' : '' }}">
+                            <a class="nav-link" href="{{ route('login') }}">Inloggen</a>
+                        </li>
+                    @endauth
+                </ul>
+            </li>
         </ul>
     </div>
 </nav>
