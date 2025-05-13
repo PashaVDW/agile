d08e18283d878a441f5f29b85a9b297cfd2b224b
ramon luteijn <ramon@intractief.nl>
2025-05-13 08:59:39 +0200

    ag-50 calendar styling

--- resources/views/user/calender/index.blade.php
+++ resources/views/user/calender/index.blade.php
@@ -4,7 +4,7 @@
 
 @section("content")
     <div class="section">
-        <div class="container">
+        <div class="container has-sidebar">
             <div class="calender-container">
                 @if ($events->isNotEmpty())
                     <div class="calender">
@@ -24,10 +24,11 @@
                         @endforeach
                     </div>
                 @endif
-                <div class="link-wrapper">
-                    <a href="{{ route('calendar.ics') }}" target="_blank" class="button is-primary is-outlined">Download</a>
-                    <a href="https://calendar.google.com/calendar/u/0?cid=NTUwYjc2YTM3N2JmNDg2MjNjYWY5MTIzMmY2ZjI1MzI0NWEyNWVkMjYzYmY3OGQ3NmVkNjIwNmJkOWEwMDNjMkBncm91cC5jYWxlbmRhci5nb29nbGUuY29t" class="button is-primary is-outlined">Google agenda</a>
-                </div>
+            </div>
+            <div class="sidebar">
+                <h1 class="title">Agenda's</h1>
+                <a href="{{ route('calendar.ics') }}" target="_blank" class="button item-button">Download</a>
+                <a href="https://calendar.google.com/calendar/u/0?cid=NTUwYjc2YTM3N2JmNDg2MjNjYWY5MTIzMmY2ZjI1MzI0NWEyNWVkMjYzYmY3OGQ3NmVkNjIwNmJkOWEwMDNjMkBncm91cC5jYWxlbmRhci5nb29nbGUuY29t" class="button item-button">Google agenda</a>
             </div>
         </div>
     </div>
