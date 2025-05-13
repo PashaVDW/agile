f5a43cd606d9c0313b96d8ed795fee6550b6b4ec
ramon luteijn <ramon@intractief.nl>
2025-05-13 11:34:23 +0200

    ag-50 added tooltip

--- resources/views/user/calender/index.blade.php
+++ resources/views/user/calender/index.blade.php
@@ -26,9 +26,20 @@
                 @endif
             </div>
             <div class="sidebar">
-                <h1 class="title">Agenda's</h1>
-                <a href="{{ route('calendar.ics') }}" target="_blank" class="button item-button">Download</a>
-                <a href="https://calendar.google.com/calendar/u/0?cid=NTUwYjc2YTM3N2JmNDg2MjNjYWY5MTIzMmY2ZjI1MzI0NWEyNWVkMjYzYmY3OGQ3NmVkNjIwNmJkOWEwMDNjMkBncm91cC5jYWxlbmRhci5nb29nbGUuY29t" class="button item-button">Google agenda</a>
+                <h2 class="has-background">Agenda's</h2>
+                <span class="tip">
+                    ?
+                    <span class="tooltiptext">
+                        Door op de Goole agenda knop te klikken wordt je automatisch doorgewezen naar je persoonlijke google agenda waar de concat agenda is toegevoegd.
+                        <br />
+                        Het downloaden van de agenda is een moment opname van de agenda op dat moment. Dit is een .ics bestand die je kan importeren in je eigen agenda.
+                    </span>
+                </span>
+                <br />
+                <div class="buttons">
+                    <a href="{{ route('calendar.ics') }}" target="_blank" class="button item-button">Download</a>
+                    <a href="https://calendar.google.com/calendar/u/0?cid=NTUwYjc2YTM3N2JmNDg2MjNjYWY5MTIzMmY2ZjI1MzI0NWEyNWVkMjYzYmY3OGQ3NmVkNjIwNmJkOWEwMDNjMkBncm91cC5jYWxlbmRhci5nb29nbGUuY29t" class="button item-button">Google agenda</a>
+                </div>
             </div>
         </div>
     </div>
