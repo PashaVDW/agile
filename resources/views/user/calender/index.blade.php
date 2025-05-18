1902a007349d09beb0169e21b130e3388425dd8e
ramon luteijn <ramon@intractief.nl>
2025-05-18 19:51:20 +0200

    scrollable nav

--- resources/views/user/calender/index.blade.php
+++ resources/views/user/calender/index.blade.php
@@ -33,10 +33,13 @@
                         Door op de Google agenda knop te klikken wordt je automatisch doorgewezen naar je persoonlijke google agenda waar de concat agenda is toegevoegd.
                         <br />
                         Het downloaden van de agenda is een moment opname van de agenda op dat moment. Dit is een .ics bestand die je kan importeren in je eigen agenda.
+                        <br />
+                        Webcal is een link die je kan toevoegen aan je agenda. Wijzigingen kunnen enkele ogenblikken duren voordat deze zichtbaar zijn in je agenda.
                     </span>
                 </span>
                 <br />
                 <div class="buttons">
+                    <button class="item-button" id="webcal">Webcal</button>
                     <a href="{{ route('calendar.ics') }}" target="_blank" class="button item-button">Download</a>
                     <a href="https://calendar.google.com/calendar/u/0?cid=NTUwYjc2YTM3N2JmNDg2MjNjYWY5MTIzMmY2ZjI1MzI0NWEyNWVkMjYzYmY3OGQ3NmVkNjIwNmJkOWEwMDNjMkBncm91cC5jYWxlbmRhci5nb29nbGUuY29t" class="button item-button">Google agenda</a>
                 </div>
