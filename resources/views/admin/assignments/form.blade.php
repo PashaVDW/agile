e87135162d22919670446ff229889a5b326bac22
Rens <rm.aarts@student.avans.nl>
2025-05-21 10:49:56 +0200

    Engels -> Nederlands

--- resources/views/admin/assignments/form.blade.php
+++ resources/views/admin/assignments/form.blade.php
@@ -10,14 +10,14 @@
                 @method('PUT')
             @endif
 
-            <x-forms.input-field type="text" name="title" label="Title" :required="true" value="{{ old('title', $assignment->title ?? '') }}" />
-            <x-forms.input-field type="text" name="company" label="Company" :required="true" value="{{ old('company', $assignment->company ?? '') }}" />
-            <x-forms.input-textarea name="description" label="Description" :required="true">{{ old('description', $assignment->description ?? '') }}</x-forms.input-textarea>
-            <x-forms.input-field type="number" name="reward" label="Reward" step="any" value="{{ old('reward', $assignment->reward ?? '') }}" />
+            <x-forms.input-field type="text" name="title" label="Titel" :required="true" value="{{ old('title', $assignment->title ?? '') }}" />
+            <x-forms.input-field type="text" name="company" label="Bedrijf" :required="true" value="{{ old('company', $assignment->company ?? '') }}" />
+            <x-forms.input-textarea name="description" label="Omschrijving" :required="true">{{ old('description', $assignment->description ?? '') }}</x-forms.input-textarea>
+            <x-forms.input-field type="number" name="reward" label="Beloning" step="any" value="{{ old('reward', $assignment->reward ?? '') }}" />
             <x-forms.input-field type="url" name="url" label="URL" :required="true" value="{{ old('url', $assignment->url ?? '') }}" />
             <x-forms.input-field type="email" name="contact_email" label="Contact Email" :required="true" value="{{ old('contact_email', $assignment->contact_email ?? '') }}" />
-            <x-forms.input-field type="text" name="contact_phone" label="Contact Phone" :required="true" value="{{ old('contact_phone', $assignment->contact_phone ?? '') }}" />
-            <x-forms.input-switch name="active" label="Active" :checked="old('active', $assignment->active ?? false)" />
+            <x-forms.input-field type="text" name="contact_phone" label="Contact Telefoonnummer" :required="true" value="{{ old('contact_phone', $assignment->contact_phone ?? '') }}" />
+            <x-forms.input-switch name="active" label="Actief" :checked="old('active', $assignment->active ?? false)" />
 
             <button type="submit" class="button right">{{ isset($assignment) ? 'Update opdracht' : 'Opdracht creëren ' }}</button>
         </form>
