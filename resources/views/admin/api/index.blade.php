74123e704a32c0d54f77f3ec1781ce6a8cbed1b5
ramon luteijn <ramon@intractief.nl>
2025-05-22 08:45:44 +0200

    #ag-32 #ag-33 simple apipage styling

--- resources/views/admin/api/index.blade.php
+++ resources/views/admin/api/index.blade.php
@@ -4,13 +4,18 @@
 
 @section("content")
     <div class="container">
-        @if(!$tokenExists)
-            <a href="{{route('admin.api.create-token')}}" class="button">Maak de eerste Weeztix connectie aan</a>
-        @endif
-        <form action="{{route('admin.api.refresh-token')}}" method="POST" enctype="multipart/form-data">
-            @csrf
-            <button type="submit" class="button">Hernieuw de Weeztix token</button>
-            <p class="hint">Gebruik deze optie alleen in geval van nood (als de bv auto refresh niet optijd refreshed). Deze token is nodig om de API te kunnen gebruiken.</p>
-        </form>
+        <div class="wrapper">
+            <h2>Weeztix</h2>
+            <div class="button-wrapper">
+                @if(!$tokenExists)
+                    <a href="{{route('admin.api.create-token')}}" class="button">Maak de eerste Weeztix connectie aan</a>
+                @endif
+                <form action="{{route('admin.api.refresh-token')}}" method="POST" enctype="multipart/form-data">
+                    @csrf
+                    <button type="submit" class="button">Hernieuw de Weeztix token</button>
+                    <p class="hint">Gebruik deze optie alleen in geval van nood (als de bv auto refresh niet optijd refreshed). Deze token is nodig om de API te kunnen gebruiken.</p>
+                </form>
+            </div>
+        </div>
     </div>
 @stop
