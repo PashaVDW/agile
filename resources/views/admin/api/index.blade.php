98bd3b3419c10f14ed890675f914b67ec59357d8
ramon luteijn <ramon@intractief.nl>
2025-05-22 12:01:49 +0200

    #ag-32 #ag-33 fixed refresh on empty bug

--- resources/views/admin/api/index.blade.php
+++ resources/views/admin/api/index.blade.php
@@ -10,11 +10,13 @@
                 @if(!$tokenExists)
                     <a href="{{route('admin.api.create-token')}}" class="button">Maak de eerste Weeztix connectie aan</a>
                 @endif
-                <form action="{{route('admin.api.refresh-token')}}" method="POST" enctype="multipart/form-data">
-                    @csrf
-                    <button type="submit" class="button">Hernieuw de Weeztix token</button>
-                    <p class="hint">Gebruik deze optie alleen in geval van nood (als de bv auto refresh niet optijd refreshed). Deze token is nodig om de API te kunnen gebruiken.</p>
-                </form>
+                @if($tokenExists)
+                    <form action="{{route('admin.api.refresh-token')}}" method="POST" enctype="multipart/form-data">
+                        @csrf
+                        <button type="submit" class="button">Hernieuw de Weeztix token</button>
+                        <p class="hint">Gebruik deze optie alleen in geval van nood (als de bv auto refresh niet optijd refreshed). Deze token is nodig om de API te kunnen gebruiken.</p>
+                    </form>
+                @endif
             </div>
         </div>
     </div>
