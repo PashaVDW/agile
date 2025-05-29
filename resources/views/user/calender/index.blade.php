838dc1bc6fba840e49eb78600be7fcf27b0a8ea3
Fenna <ikkie857@outlook.com>
2025-05-29 14:16:27 +0200

    Kalender styling fixes

--- resources/views/user/calender/index.blade.php
+++ resources/views/user/calender/index.blade.php
@@ -28,29 +28,29 @@
             <div class="sidebar">
                 @if(request('status') === 'my_events')
                     <h2 class="has-background">Eigen Agenda's</h2>
-                    <p>Gefilterd op eigen activiteiten. Dus alleen deze zullen op het moment aan uw eigen agenda worden toegevoegd.</p>
+                    <p>Gefilterd op eigen activiteiten. Dus alleen de activiteiten waarvoor u bent ingeschreven zullen aan uw eigen agenda worden toegevoegd.</p>
                 @else
                     <h2 class="has-background">Agenda's</h2>
                 @endif
                 <span class="tip">
                     ?
                     <span class="tooltiptext">
+                        <strong>Webcal</strong> is een link die je kan toevoegen aan je agenda. Wijzigingen kunnen enkele ogenblikken duren voordat deze zichtbaar zijn in je agenda.
+                        <br />
                         @if(request('status') === null)
-                            Door op de Google agenda knop te klikken wordt je automatisch doorgewezen naar je persoonlijke google agenda waar de concat agenda is toegevoegd.
+                            Door op de <strong>Google agenda</strong> knop te klikken wordt je automatisch doorgewezen naar je persoonlijke Google agenda waar de concat agenda is toegevoegd.
                             <br />
                         @endif
-                        Het downloaden van de agenda is een moment opname van de agenda op dat moment. Dit is een .ics bestand die je kan importeren in je eigen agenda.
-                        <br />
-                        Webcal is een link die je kan toevoegen aan je agenda. Wijzigingen kunnen enkele ogenblikken duren voordat deze zichtbaar zijn in je agenda.
+                        Het <strong>downloaden</strong> van de agenda is een moment opname van de agenda op dat moment. Dit is een .ics bestand die je kan importeren in je eigen agenda.
                     </span>
                 </span>
                 <br />
                 <div class="buttons {{request('status') === 'my_events' ? 'my_events' : ''}}">
                     <button class="item-button" id="webcal" data-user-id="{{$user}}">Webcal</button>
-                    <a href="{{ route('calendar.ics', ['status' => request('status')]) }}" target="_blank" class="button item-button">Download</a>
                     @if(request('status') === null)
                         <a href="https://calendar.google.com/calendar/u/0?cid=NTUwYjc2YTM3N2JmNDg2MjNjYWY5MTIzMmY2ZjI1MzI0NWEyNWVkMjYzYmY3OGQ3NmVkNjIwNmJkOWEwMDNjMkBncm91cC5jYWxlbmRhci5nb29nbGUuY29t" class="button item-button">Google agenda</a>
                     @endif
+                    <a href="{{ route('calendar.ics', ['status' => request('status')]) }}" target="_blank" class="button item-button">Download</a>
                 </div>
                 <br />
                 @if(auth()->check())
