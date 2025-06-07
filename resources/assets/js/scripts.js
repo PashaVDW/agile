e5bcf37f8c4a3d0656240c0e1a1319def8878399
Rens <rm.aarts@student.avans.nl>
2025-06-07 11:38:08 +0200

    Swiper voor de homepagina aangepast, gelijk ook de spacing meegenomen van #ag-137.

--- resources/assets/js/scripts.js
+++ resources/assets/js/scripts.js
@@ -24,9 +24,10 @@ document.addEventListener('DOMContentLoaded', function() {
 function carouselSwiper(swiperId) {
     new Swiper(swiperId, {
         slidesPerView: 1,
-        spaceBetween: swiperId === '#boardSwiper' ? 20: 0,
+        spaceBetween: swiperId === '#boardSwiper' ? 20 : 5,
         speed: 300,
         loop: true,
+        direction:swiperId === '#homeSwiper' ? 'vertical' : 'horizontal',
         autoplay: {
             delay: 6000,
         },
@@ -39,16 +40,15 @@ function carouselSwiper(swiperId) {
         },
         breakpoints: {
             500: {
-                slidesPerView: 2,
+                slidesPerView: swiperId === '#homeSwiper' ?  1 : 2,
             },
             768: {
-                slidesPerView: 3,
+                slidesPerView: swiperId === '#homeSwiper' ?  1 : 3,
             },
             980: {
-                slidesPerView: swiperId === '#homeSwiper' ? 2 : 3,
+                slidesPerView: swiperId === '#homeSwiper' ? 1 : 3,
             }
         }
-
     });
 }
 
